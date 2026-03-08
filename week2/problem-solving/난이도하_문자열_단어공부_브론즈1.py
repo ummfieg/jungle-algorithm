a448ac99fb6d6e2b8e3b20fea68c393229419e60
ummfieg <ummfieg@naver.com>
2026-03-08 15:09:53 +0900

    [boj] 1157: 단어공부

--- week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
+++ week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
@@ -1,2 +1,22 @@
 # 문자열 - 단어 공부 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/1157
+
+def many_word():
+    word = str(input())
+    up_word = (word.upper())
+    peice = {}
+    for w in up_word:
+        if w not in peice:
+            peice[w] = 1
+        else:
+            peice[w] += 1
+
+    result = [k for k,v in peice.items() if max(peice.values()) == v]
+    if (len(result) > 1):
+        print("?")
+    else:
+        print(result[0])
+
+many_word()
+
+      
