1eb1877d371488a35f8ae0400c5786ffc3c1b55d
ummfieg <ummfieg@naver.com>
2026-03-08 15:20:08 +0900

    [boj] 4344: 평균은 넘겠지

--- week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
+++ week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
@@ -1,2 +1,23 @@
 # 배열 - 평균은 넘겠지 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/4344
+
+def sad_truth():
+    n = int(input())
+
+    for _ in range(n):
+        data = list(map(int, input().split()))
+        
+        student = data[0]
+        scores = data[1:]
+        
+        avg = sum(scores) / student
+        
+        count = 0
+        for score in scores:
+            if score > avg:
+                count += 1
+        
+        percent = (count / student) * 100
+        print(f"{percent:.3f}%")
+    
+sad_truth()
