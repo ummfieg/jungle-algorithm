188a4485276ba9f36eb78da67c2646c9b437f86b
ummfieg <ummfieg@naver.com>
2026-03-08 17:20:54 +0900

    [boj] 2675: 문자열반복

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,18 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+
+def repeat_str():
+    test_count = int(input())
+    for _ in range(test_count):
+        result = []
+        data = list(map(str, input().split()))
+        repeat_count = int(data[0])
+        text = data[1:] 
+        for w in text[0]:
+            texts = (w * repeat_count)
+            result.append(texts)
+
+        results ="".join(result)
+        print(results)
+        
+repeat_str()
