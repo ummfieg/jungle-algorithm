542e845be76e4bb06365663171aad2154b31cf10
ummfieg <ummfieg@naver.com>
2026-03-10 13:40:22 +0900

    백트래킹 - 조합생성 solve

--- week2/basic/06_backtracking.py
+++ week2/basic/06_backtracking.py
@@ -22,7 +22,8 @@
 - 현재 숫자보다 큰 숫자만 선택하여 중복 방지
 """
 
-def combinations(n, k):
+def combinations(n, k): #wrapper
+   
     """
     1부터 n까지 숫자 중 k개를 선택하는 모든 조합 찾기
     
@@ -34,7 +35,7 @@ def combinations(n, k):
         모든 조합의 리스트
     """
     result = []
-    
+
     def backtrack(start, current_combination):
         """
         백트랙킹 헬퍼 함수
@@ -44,14 +45,20 @@ def combinations(n, k):
             current_combination: 현재까지 선택한 숫자들
         """
         # TODO: base case - k개를 모두 선택했으면 결과에 추가
-        pass
+        if len(current_combination) == k:
+            result.append(current_combination[:])
+            return result
         
+        for i in range(start, n+1):
+            current_combination.append(i)
+            backtrack(i+1, current_combination)
+            current_combination.pop()
+
         # TODO: start부터 n까지 숫자를 하나씩 시도
         ## TODO: 백트랙킹 3단계 구현
-        ## 1. 선택(Choose)
-        ## 2. 탐색(Explore)
-        ## 3. 취소(Unchoose)
-        pass
+        ## 1. 선택(Choose) current_combination.append(i)
+        ## 2. 탐색(Explore) backtrack(i+1, current_combination)
+        ## 3. 취소(Unchoose) current_combination.pop()
     
     backtrack(1, [])
     return result
