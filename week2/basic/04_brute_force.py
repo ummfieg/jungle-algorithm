0ae7078a2651e96e28f52242287069cb2e81650b
ummfieg <ummfieg@naver.com>
2026-03-10 10:12:52 +0900

    배열에서 두 수의 합 찾기 solve

--- week2/basic/04_brute_force.py
+++ week2/basic/04_brute_force.py
@@ -37,15 +37,20 @@ def find_two_sum_pairs(nums, target):
     Returns:
         list: (i, j) 인덱스 쌍의 리스트
     """
+    
     pairs = []
     n = len(nums)
+    
+    for i in range(n-1):
+        for j in range(i+1 ,n): 
+            if nums[i]+nums[j] == target:
+                pairs.append((i,j))
     
     # TODO: 이중 반복문으로 모든 쌍을 확인하세요
     ## 외부 반복문: i는 0부터 n-1까지
     ## 내부 반복문: j는 i+1부터 n까지 (중복 방지)
     ## nums[i] + nums[j]가 target과 같으면 (i, j)를 결과에 추가
-    pass  
-    
+
     return pairs
 
 # 테스트 케이스
