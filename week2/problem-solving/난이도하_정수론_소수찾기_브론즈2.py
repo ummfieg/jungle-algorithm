3cd559f1b1dcf1ba5b82e271214032afd69b9ea3
ummfieg <ummfieg@naver.com>
2026-03-13 00:29:49 +0900

    [boj] 1978: 소수찾기

--- week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
+++ week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
@@ -1,2 +1,30 @@
 # 정수론 - 소수 찾기 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/1978
+
+def count_prime():
+   n = int(input())
+ 
+   nums = list(map(int, input().split()))
+   
+   count =0
+   for num in nums:
+      if  num < 2:
+        continue
+
+      is_prime = True
+
+      for j in range(2, num):
+            if num % j == 0:
+               is_prime= False
+               break
+
+      if is_prime:
+         count += 1
+
+   print(count)
+count_prime()
+
+
+
+
+                  
