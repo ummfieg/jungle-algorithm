488e36081da1d388f719431fbf4feac97a9763d6
ummfieg <ummfieg@naver.com>
2026-03-13 17:51:15 +0900

    이진탐색 solve

--- week3/basic/01_binary_search.py
+++ week3/basic/01_binary_search.py
@@ -36,6 +36,20 @@ def binary_search(arr, target):
     left = 0
     right = len(arr) - 1
     
+    while left <= right:
+        mid = (left+right) //2
+        
+        if arr[mid] > target:
+            right = mid - 1
+            mid = (left+right) //2
+        elif arr[mid] < target:
+            left = mid +1
+
+        if arr[mid] == target:
+            return mid
+        
+        
+        
     # TODO: left가 right보다 작거나 같을 때까지 반복
     ## 중간 인덱스 계산
     ## arr[mid]와 target 비교
