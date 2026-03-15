7cad3ef68799731de50cd83254212e324f1be642
ummfieg <ummfieg@naver.com>
2026-03-15 19:57:03 +0900

    [boj] 10828: 스택-스택

--- week3/problem-solving/난이도하_스택_스택_실버4.py
+++ week3/problem-solving/난이도하_스택_스택_실버4.py
@@ -1,2 +1,32 @@
 # 스택 - 스택 (백준 실버 4)
 # 문제 링크: https://www.acmicpc.net/problem/10828
+def stack_input():
+    run_count = int(input())
+
+    stack = []
+
+    data = ([str(input()) for _ in range(run_count)])
+
+    for i in data:
+        if "push" in i:
+            push_data = i.split()
+            stack.append(push_data[1])
+        elif i == "top":
+            if len(stack) >= 1:
+                print(stack[-1])
+            else:
+                print(-1)
+        elif i == "size":
+            print(len(stack))
+        elif i == "empty":
+            if len(stack) <= 0:
+                print(1)
+            else:
+                print(0)
+        elif i == "pop":
+            if len(stack) >= 1:
+                pop_value = stack.pop()
+                print(pop_value)
+            else:
+                print(-1)
+stack_input()
