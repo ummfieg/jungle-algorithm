f28ca52e8dd03cfca034e5b51f2354012040b665
ummfieg <ummfieg@naver.com>
2026-03-15 23:41:11 +0900

    [boj] 2164: 카드2

--- week3/problem-solving/난이도하_큐_카드2_실버4.py
+++ week3/problem-solving/난이도하_큐_카드2_실버4.py
@@ -1,2 +1,20 @@
 # 큐 - 카드2 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/2164
+
+from collections import deque
+
+def card_game():
+    count = int(input())
+    queue = deque()
+
+    for i in range(1,count+1):
+        queue.append(i)
+    
+    while len(queue) > 1:
+        queue.popleft()
+        value = queue.popleft()
+        queue.append(value)
+
+    print(queue.pop())
+    
+card_game()
